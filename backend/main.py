d09e5e6c736e9c7df2803f9c965a0afebd9c6039
Brandon <brandonhernandez_10@outlook.es>
2022-09-29 12:12:33 -0600

    error message

--- backend/main.py
+++ backend/main.py
@@ -15,6 +15,14 @@ GAMES = {}
 database = mongo.connect()
 roomService = Rooms(database)
 
+def sendError(websocket, response):
+  try:
+    _, connected = GAMES[roomID]
+    print('[SERVER] Sending to', _.roomID, ':', response)
+    websockets.broadcast(connected, json.dumps(response))
+  except Exception as e:
+    print('[ERROR] SEND ENRROR:', e)
+
 def broadcast(roomID, response):
   try:
     _, connected = GAMES[roomID]
@@ -22,6 +30,15 @@ def broadcast(roomID, response):
     websockets.broadcast(connected, json.dumps(response))
   except Exception as e:
     print('[ERROR] BROADCAST:', e)
+
+def sendMessage(websocket, roomID, response):
+  try:
+    if (response['type'] == 'error'):
+      sendError(websocket, response)
+    else:
+      broadcast(roomID, response)
+  except Exception as e:
+    print('[ERROR] SEND:', e)
 
 async def roomHandler(request, websocket):
   try:
@@ -196,13 +213,13 @@ async def sessionHandler(websocket):
         # Check the request type
         if (request['type'] == 'room'):
           response = await roomHandler(request, websocket)
-          broadcast(request['roomID'], response)
+          sendMessage(websocket, request['roomID'], response)
         elif (request['type'] == 'game'):
           response = await gameHandler(request)
-          broadcast(request['roomID'], response)
+          sendMessage(websocket, request['roomID'], response)
         elif (request['type'] == 'chat'):
           response = await chatHandler(request)
-          broadcast(request['roomID'], response)
+          sendMessage(websocket, request['roomID'], response)
 
       except websockets.ConnectionClosedOK as e:
         print('[SERVER] Websocket closed', e)
