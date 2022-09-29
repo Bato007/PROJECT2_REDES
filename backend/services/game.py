b7937e82ceec4154537642538f69cd1a0ba38c0c
Brandon <brandonhernandez_10@outlook.es>
2022-09-28 22:18:49 -0600

    Change src of images

--- backend/services/game.py
+++ backend/services/game.py
@@ -7,6 +7,11 @@ class Game(object):
     self.users = users
     self.usersDeck = deck_user
   
+  def getNewTurns(self, turn):
+    i = self.users.index(turn) + 1
+    self.users = self.users[i:] + self.users[:i]
+    self.turns = itertools.cycle(self.users)
+
   def addNewUser(self, user):
     self.users.append(user)
   
@@ -23,12 +28,41 @@ class Game(object):
   def drawCard(self, username):
     drawedCard = self.deck.pop(0)
 
+    # It's not a bomb
     if (drawedCard != 18):
       self.usersDeck[username].append(drawedCard)
       return {
         'card': drawedCard,
         'username': username,
         'turn': next(self.turns),
+        'lost': False,
       }
+    
+    # It's a bomb
+    userDeck = self.usersDeck[username]
+
+    # Check for defuse
+    index = -1
+    for i in range(len(userDeck)):
+      if (userDeck[i].id == 19):
+        index = i
+        break
+    
+    # User lost the game
+    if (index == -1):
+      # Removes this user
+      del self.usersDeck[username]
+      self.users.remove(username)
+
+      turn = next(self.turns)
+      self.getNewTurns(turn)  # Generates new turn
+
+      return {
+        'card': drawedCard,
+        'username': username,
+        'turn': turn,
+        'lost': True,
+      }
+
 
     print('holaaa')
