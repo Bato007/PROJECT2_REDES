21bc41bde26e7f222855e0df0c6bde05e5ce8ce9
Brandon <brandonhernandez_10@outlook.es>
2022-09-29 20:57:01 -0600

    pem

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 import websockets
 import json, asyncio
+import pathlib
+import ssl
 
 # Import services
 import mongo
@@ -306,8 +308,11 @@ async def sessionHandler(websocket):
         continue
 
 async def main():
-    async with websockets.serve(sessionHandler, "", PORT):
-      await asyncio.Future()  # run forever
+  ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+  ssl_context.load_cert_chain(
+    pathlib.Path(__file__).with_name('localhost.pem'))
+  async with websockets.serve(sessionHandler, "", PORT, ssl=ssl_context):
+    await asyncio.Future()  # run forever
 
 if __name__ == "__main__":
   print('[SERVER] Started')
